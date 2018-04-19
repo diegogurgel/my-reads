d96219be25240af4dc593203d21ba2cda84a1696
Diego Gurgel <diego@diegogurgel.com>
2018-04-19 00:04:00 -0300

    Add browser router and 'add button'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Shelf from "./Shelf";
-import {getAll as getBooks} from './BooksAPI';
+import { getAll as getBooks } from './BooksAPI';
+import './App.css';
+
 
 class App extends Component {
   state = {
@@ -22,11 +24,20 @@ class App extends Component {
     const currentlyReading = this.state.books.filter(book => book.shelf === 'currentlyReading')
     const read = this.state.books.filter(book => book.shelf === 'read')
     return (
-      <div className="App">
-        <Shelf title="Currently Reading" onBookChanged={this.onBookChanged} books={currentlyReading}/>
-        <Shelf title="Want to Read" onBookChanged={this.onBookChanged} books={wantToRead} />
-        <Shelf title="Read" onBookChanged={this.onBookChanged} books={read} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact render={() => (
+            <div className="app">
+              <Shelf title="Currently Reading" onBookChanged={this.onBookChanged} books={currentlyReading} />
+              <Shelf title="Want to Read" onBookChanged={this.onBookChanged} books={wantToRead} />
+              <Shelf title="Read" onBookChanged={this.onBookChanged} books={read} />
+              <Link to="/add-book" className="add"></Link>
+            </div>
+
+          )} />
+          <Route path="/add-book" />
+        </div>
+      </BrowserRouter>
     );
   }
 }
